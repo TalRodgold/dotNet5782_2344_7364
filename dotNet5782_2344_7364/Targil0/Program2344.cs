f85a5a738f09656b3a9a1644b5b182782e2aced0
tal rodgold <talrodgold@gmail.com>
2021-10-07 14:52:22 +0300

    Initial code + welcome functions

---
+++ dotNet5782_2344_7364/Targil0/Program2344.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome2344();
+            welcome7364();
+            Console.ReadKey();
+        }
+
+        private static void welcome2344()
+        {
+            Console.Write("Enter your name: ");
+            string user_name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", user_name);
+        }
+
+        static partial void welcome7364();
+    }
+}
