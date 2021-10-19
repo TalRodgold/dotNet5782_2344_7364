7f5946acb5386364ee1f828950ee61b43a0d64bc
tal rodgold <talrodgold@gmail.com>
2021-10-19 17:07:59 +0300

    test for bug

--- dotNet5782_2344_7364/ConsoleUI/Program.cs
+++ dotNet5782_2344_7364/ConsoleUI/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IDAL.DO;
 namespace ConsoleUI
 {
     class Program
@@ -14,7 +15,13 @@ namespace ConsoleUI
             switch (User_input)
             {
                 case (1):
-                    Console.WriteLine("Please enter following data: \n 1) ");
+                    Console.WriteLine("Please enter following data: \n 1) Id \n 2) Name \n 3) ChargeSlots \n 4) Longtitude \n 5) Latitude ");
+                    int User_id = Console.Read();
+                    string User_name = Console.ReadLine();
+                    int User_chargeSlots = Console.Read();
+                    double User_longtitude = Console.Read();
+                    double User_latitude = Console.Read();
+                    
                     break;
                 default:
                     break;
