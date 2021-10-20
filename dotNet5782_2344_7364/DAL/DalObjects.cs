6e8539feacfbbf4f2925211826c2cc407e4afe6b
tal rodgold <talrodgold@gmail.com>
2021-10-20 09:11:38 +0300

    i added a constructor for basestation

--- dotNet5782_2344_7364/DAL/DalObjects.cs
+++ dotNet5782_2344_7364/DAL/DalObjects.cs
@@ -7,5 +7,24 @@ using IDAL.DO;
 
 namespace DalObjects
 {
-    
+    public class DalObjects
+    {
+        public DalObjects() // constructor
+        {
+            DataSource.Initialize();
+        }
+        public static void ConstructBaseStation(int id, string name, int chargeSlots, double longtitude, double latitude)
+        {
+            BaseStation new_base_station = new BaseStation
+            {
+                Id = id,
+                Name = name,
+                ChargeSlots = chargeSlots,
+                Longtitude = longtitude,
+                Latitude = latitude
+            };
+            DataSource.BaseStation_arr[DataSource.Config.BaseStation_arr_index] = new_base_station;
+            DataSource.Config.BaseStation_arr_index++;
+        }
+    }
 }
