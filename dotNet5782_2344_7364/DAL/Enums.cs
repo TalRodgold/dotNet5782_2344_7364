193e656c8e844974ad4b2bd524855b4833473ed3
tal rodgold <talrodgold@gmail.com>
2021-10-20 15:27:17 +0300

    test

--- dotNet5782_2344_7364/DAL/Enums.cs
+++ dotNet5782_2344_7364/DAL/Enums.cs
@@ -7,15 +7,15 @@ namespace IDAL
 {
     namespace DO
     {
-        public enum WeightCategories
+        public enum WeightCategories // 3 types of weight categories
         { Light, Medium, Heavy }
-        public enum Priorities 
+        public enum Priorities  // 3 types of proirities
         { Regular,Express,Urgent }
-         public enum DroneStatuses
+         public enum DroneStatuses // 3 types of Drone status
         { Available, Delivery, Maintenance }
-        public enum RandomBases
+        public enum RandomBases // random list of basses names
         { Gilo, Baaka, Pissgat_zev, Katamon, Malha, Givat_shaul, City_center, San_simon, Givat_mordehai, Arnona}
-        public enum RandomNames
+        public enum RandomNames // random list of names
         { Yossi, Moti, Avraham, Kobi, David, Haim, Hana, Shoshi, Malkishua, Yossef, Hagit, Yarden, Erez, Yoni, Gali }
 
     }
